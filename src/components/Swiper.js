848a77adc90876a5a521ad454a8d15a4fb59ad63
Kypso18 <skipperpaloma@gmail.com>
2023-07-07 01:53:54 +0800

    update Swiper.js

--- src/components/Swiper.js
+++ src/components/Swiper.js
@@ -29,7 +29,7 @@ const SwiperContainer = () => {
     },
     // When window width is >= 768px
     768: {
-      slidesPerView: 1,
+      slidesPerView: 2,
     },
     // When window width is >= 1024px
     1024: {
