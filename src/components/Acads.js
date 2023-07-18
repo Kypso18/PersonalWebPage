d4812b2c96b76c951e33a31b921ee6948a6d1116
Kypso18 <skipperpaloma@gmail.com>
2023-07-18 23:27:24 +0800

    update Acads.js

--- src/components/Acads.js
+++ src/components/Acads.js
@@ -26,18 +26,18 @@ const AcademicBackgroundSection = () => {
     <section className="py-8">
       <div className="container mx-auto px-12 xl:px-48 md:px-24 lg:px-32 sm:px-20">
         <h2 className="text-2xl font-bold mb-4 text-center">Academic Background</h2>
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
+        <div className="grid gap-4 md:grid-cols lg:grid-cols-2">
           {academicBackground.map((academic) => (
             <div key={academic.id} className="bg-white/50 rounded-lg p-4 shadow-md">
               <div className="flex items-center mb-2">
                 <FaGraduationCap className="w-6 h-6 mr-3" />
                 <div>
-                  <p className="text-md lg:text-lg font-bold">{academic.degree}</p>
-                  <p className="text-gray-600 text-sm lg:text-md">{academic.institution}</p>
-                  <p className="text-gray-600 text-sm lg:text-md">Graduated in {academic.graduationYear}</p>
+                  <p className="text-sm font-bold">{academic.degree}</p>
+                  <p className="text-gray-600 text-sm">{academic.institution}</p>
+                  <p className="text-gray-600 text-sm">Graduated in {academic.graduationYear}</p>
                 </div>
               </div>
-              <p className="text-gray-800 text-sm lg:text-md px-3">&nbsp; &nbsp;&nbsp;{academic.description}</p>
+              <p className="text-gray-800 text-sm px-3">&nbsp; &nbsp;&nbsp;{academic.description}</p>
             </div>
           ))}
         </div>
